1f9d291955a49c11e57f01265a86d91b9835cb73
Derek Straka <derek@asterius.io>
2024-09-19 12:28:49 +0200

    Replace deprecated env variable (BB_ENV_EXTRAWHITE) with the new value of BB_ENV_PASSTHROUGH_ADDITIONS
    Signed-off-by: Derek Straka <derek@asterius.io>

--- bitbake.sh
+++ bitbake.sh
@@ -5,4 +5,4 @@ set -eo pipefail
 cmd=$(basename "$0")
 
 setuser builduser \
-	bash -c "source /var/build/poky/oe-init-build-env && export BB_ENV_EXTRAWHITE=\"${BB_ENV_EXTRAWHITE} MACHINE SSTATE_DIR SSTATE_MIRRORS SSH_AUTH_SOCK\" && ${cmd} $*"
+	bash -c "source /var/build/poky/oe-init-build-env && export BB_ENV_PASSTHROUGH_ADDITIONS=\"${BB_ENV_PASSTHROUGH_ADDITIONS} MACHINE SSTATE_DIR SSTATE_MIRRORS SSH_AUTH_SOCK\" && ${cmd} $*"
